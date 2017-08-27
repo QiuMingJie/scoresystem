3012d4e33e126f46401a2846737d4edf543e39a2
CollinSon <13538350844@163.com>
2017-08-27 19:25:13 +0800

    更改了配置，增加了数据库表前缀

--- conf/buildDB.sql
+++ conf/buildDB.sql
@@ -8,20 +8,33 @@ create table if not exists score_admin_users(
 id int(8) unsigned not null auto_increment primary key,
 email varchar(255) default null comment '邮箱',
 name  varchar(255) not null comment '姓名',
-password varchar(255) not null comment '密码');
+password varchar(255) not null comment '密码');/*最高级管理员表*/
 
 insert into score_admin_users (id,email,name,password) values ('1','','admin','admin');
 
 drop table if exists score_teachers;
 
-create table not exists score_teachers(
+create table if not exists score_teachers(
 id int(8) unsigned not null auto_increment primary key,
 email varchar(255) default null comment '邮箱',
 name varchar(255) not null comment '姓名',
-password varchar(255) not null comment '密码');
+password varchar(255) not null comment '密码'
+aca varchar(255) not null comment '学院名');/*管理员表*/
 
-create table not exists score_student_users(
-id int(8) unsigned not null auto_increment primary key,
+drop table if exists score_student_users;
+
+create table if not exists score_student_users(
+id int(255) unsigned not null auto_increment primary key,
 number int(255) unsigned not null unique key comment '学号',
 name varchar(255) not null comment '姓名',
-password varchar(255) not null comment '密码');
+password varchar(255) not null comment '密码');/*学生用户表*/
+
+drop table if exists score_student_projects;
+
+create table if not exists score_student_projects(
+id int(255)unsigned not null auto_increment primary key,
+name varchar(255) not null comment '项目名',
+time varchar(255) not null comment '项目时间',
+type varchar(255) not null comment '项目类型',
+mark double(2,1) not null comment '项目分',
+student_id int(255) not null comment '学生ID');/*项目表*/
