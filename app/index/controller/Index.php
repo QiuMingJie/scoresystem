d79eca951fc25470c68deb91ab19578dd27917ac
CollinSon <13538350844@163.com>
2017-09-04 19:07:22 +0800

    实现了登录逻辑，增加了AJAX借口

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Request;
+use think\Db;
 
 class Index extends \think\Controller
 {
@@ -10,6 +11,19 @@ class Index extends \think\Controller
 		return $this->fetch();
     }
 	public function login(){
-		return 'yes';
+		$username=Request::instance()->param('username');
+		$password=Request::instance()->param('password');
+		$status=Request::instance()->param('is_stu');
+		$status=($status==true)?"student":"teacher";
+		$table=$status.'_users';
+		$usercolumn=($status=='student')?'number':'name';
+		$result=Db::name($table)->where($usercolumn,$username)->where('password',$password)->find();
+		
+		if($result){
+			return json_encode($result);
+		}
+		else{
+			return 'no this man';
+		}
 	}
 }
